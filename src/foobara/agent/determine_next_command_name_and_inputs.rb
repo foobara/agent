d630a2b7bbe9f55b689112511a949ebbf95c78d7
Miles Georgi <azimux@gmail.com>
2025-07-14 07:34:45 -0700

    Eliminate DetermineBase

--- src/foobara/agent/determine_next_command_name_and_inputs.rb
+++ src/foobara/agent/determine_next_command_name_and_inputs.rb
@@ -1,8 +1,8 @@
-require_relative "determine_base"
+require "foobara/llm_backed_command"
 
 module Foobara
   class Agent < CommandConnector
-    class DetermineNextCommandNameAndInputs < DetermineBase
+    class DetermineNextCommandNameAndInputs < Foobara::LlmBackedCommand
       class << self
         def llm_instructions(assistant_association_depth, goal, previous_goals = nil)
           key = [assistant_association_depth, goal, previous_goals]
@@ -61,12 +61,80 @@ module Foobara
         inputs :attributes, :allow_nil
       end
 
+      inputs do
+        agent Agent, :required
+        pass_aggregates_to_llm :boolean, :allow_nil, "Should we send aggregates to the LLM or " \
+                                                     "require it to fetch what it needs?"
+        llm_model :string,
+                  one_of: Foobara::Ai::AnswerBot::Types::ModelEnum,
+                  default: Ai.default_llm_model,
+                  description: "The model to use for the LLM"
+      end
+
       def determine_llm_instructions
         self.llm_instructions = self.class.llm_instructions(
           computed_user_association_depth,
           goal,
           previous_goal_and_status_pairs
         )
+      end
+
+      def association_depth
+        if pass_aggregates_to_llm
+          Foobara::AssociationDepth::AGGREGATE
+        else
+          Foobara::AssociationDepth::ATOM
+        end
+      end
+
+      def build_messages
+        p = [
+          {
+            content: llm_instructions,
+            role: :system
+          }
+        ]
+
+        context.command_log.each do |command_log_entry|
+          agent_entry = {
+            command: command_log_entry.command_name
+          }
+
+          inputs = command_log_entry.inputs
+
+          if inputs && !inputs.empty?
+            agent_entry[:inputs] = inputs
+          end
+
+          outcome_entry = command_log_entry.outcome
+
+          p << {
+            content: agent_entry,
+            role: :assistant
+          }
+          p << {
+            content: outcome_entry,
+            role: :user
+          }
+        end
+
+        p
+      end
+
+      def goal
+        context.current_goal.text
+      end
+
+      def previous_goal_and_status_pairs
+        if context.previous_goals && !context.previous_goals.empty?
+          context.previous_goals.map do |previous_goal|
+            [previous_goal.text, previous_goal.state]
+          end
+        end
+      end
+
+      def context
+        agent.context
       end
     end
   end
